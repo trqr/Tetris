1e70b8cf14a57959493582df1b103c9a78c16d54
TrqR <tristanqr@gmail.com>
2025-05-27 21:14:39 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,160 @@
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
+
+
+const gridSize = 20;
+const widthTileCount = canvas.width / gridSize; // 20
+const heightTileCount = canvas.height / gridSize; // 30
+const offsetX = (widthTileCount * gridSize / 2) - 20;
+const offsetY = 0;
+let currentShape;
+let landedBlocks = [[0,29],[1,29],[2,29],[3,29],[4,29],[5,29],[6,29],[7,29]]
+
+const startBtn = document.querySelector('#start-btn');
+
+
+
+const tetrisShapes = {
+    I: { color: 'cyan',
+        blocks: [[1,0],[1,1],[1,2],[1,3]]
+    },
+    O: { color: 'yellow',
+        blocks: [[0,0], [0,1],[1,0],[1,1]]
+    },
+    T: { color: 'purple',
+        blocks: [[1,0],[0,1],[1,1],[2,1]]
+    },
+    S: { color: 'green',
+        blocks: [[1,0],[2,0],[0,1],[1,1]]
+    },
+    Z: { color: 'red',
+        blocks: [[0,0],[1,0],[1,1],[2,1]]
+    },
+    J: { color: 'blue',
+        blocks: [[0,0],[0,1],[1,1],[2,1]]
+    },
+    L: { color: 'orange',
+        blocks: [[2,0],[0,1],[1,1],[2,1]]
+    }
+}
+
+
+//init
+
+drawRdmBlock()
+startBtn.addEventListener('click', gameLoop())
+
+function drawBlock(x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, gridSize, gridSize);
+  ctx.strokeStyle = 'black';
+  ctx.strokeRect(x, y, gridSize, gridSize);
+}
+
+function drawRdmBlock(){
+    const keysArray = Object.keys(tetrisShapes)
+    const rdmNb = Math.floor(Math.random() * keysArray.length);
+    const rdmKey = keysArray[rdmNb];
+    tetrisShapes[rdmKey];
+    const shape = tetrisShapes[rdmKey];
+
+    for (const [x, y] of shape.blocks){
+        drawBlock(offsetX + x * gridSize, offsetY + y * gridSize, shape.color);
+    }
+
+    currentShape = structuredClone(shape);
+}
+
+
+
+
+// //Keypress events
+document.addEventListener('keydown', (e) => {
+    if (e.key === "ArrowLeft" || e.key === "q"){
+        if (!currentShape.blocks.find(block => block[0] < -8)){
+            currentShape.blocks.forEach(block => {
+                block[0]-= 1;
+                draw()
+        });
+        }
+    }
+    if (e.key === "ArrowRight" || e.key === "d"){
+        if (!currentShape.blocks.find(block => block[0] > 9)){
+            currentShape.blocks.forEach(block => {
+                    block[0]+= 1;
+                    draw();
+            });
+        }
+    }
+    if (e.key === "ArrowDown" || e.key === "s"){
+        currentShape.blocks.forEach(block => {
+            block[1]+= 1;
+            checkColision();
+            checkIfLanded();
+            draw();
+        }); 
+    }
+});
+
+
+function gameLoop(){
+    currentShape.blocks.forEach(block => {
+        block[1]+= 1;
+        draw();
+    });
+    checkColision();
+    checkIfLanded();
+    checkAndEraseLine();
+    setTimeout(gameLoop, 400);
+    draw();
+}
+
+function checkIfLanded(){
+    if (currentShape.blocks.find((block => block[1] === heightTileCount - 1))){
+        currentShape.blocks.forEach(block => landedBlocks.push(block));
+        drawRdmBlock();
+    }
+    
+}
+
+function checkColision(){
+    for (let i =0; i < currentShape.blocks.length; i++){
+        if (landedBlocks.find(bloc => (bloc[0] === currentShape.blocks[i][0]) && (bloc[1] === currentShape.blocks[i][1] + 1))){
+            currentShape.blocks.forEach(block => landedBlocks.push(block));
+            drawRdmBlock();
+            break;
+        }
+    }
+}
+
+function checkAndEraseLine() {
+  for (let i = 1; i <= heightTileCount; i++) {
+    if (landedBlocks.filter(block => block[1] === i).length >= widthTileCount) {
+      landedBlocks = landedBlocks.filter(block => block[1] !== i);
+      landedBlocks = landedBlocks.map(block => {
+        if (block[1] < i) {
+          return [block[0], block[1] + 1];
+        }   
+        return block;
+      });
+    }
+  }
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    currentShape.blocks.forEach(block => {
+        ctx.fillStyle = currentShape.color;
+        ctx.fillRect((block[0]* gridSize) + offsetX , (block[1] * gridSize), gridSize, gridSize);
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect((block[0] * gridSize) + offsetX, (block[1] * gridSize), gridSize, gridSize);
+    })
+
+    landedBlocks.forEach(block => {
+            ctx.fillStyle = "grey"
+            ctx.fillRect((block[0]* gridSize) + offsetX , (block[1] * gridSize), gridSize, gridSize);
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect((block[0] * gridSize) + offsetX, (block[1] * gridSize) , gridSize, gridSize);
+        })
+}
